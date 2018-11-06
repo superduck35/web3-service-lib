660461b0f4d35f28c49d64a44261db08abc4607e
alsco77 <alsco77@pm.me>
2018-11-06 12:33:49 +0800

    Updated service to accomodate new metamask changes

--- projects/web3-service-lib/src/lib/eth.service.ts
+++ projects/web3-service-lib/src/lib/eth.service.ts
@@ -1,12 +1,15 @@
 import { Inject, Injectable, OnDestroy } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import merge from 'lodash.merge';
 import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
+
+import { Step } from '../canpay-wizard/canpay-wizard.component';
 
 declare let require: any;
 const Web3 = require('web3');
-declare var web3;
+declare var window;
 
-const gas = '210000';
+const gas = { gasPrice: '8000000000', gas: '210000' };
 const gasStationApi = 'https://ethgasstation.info/json/ethgasAPI.json';
 
 export enum WalletType {
@@ -18,41 +21,45 @@ export enum NetworkType {
   main = 'Mainnet',
   ropsten = 'Ropsten',
   rinkeby = 'Rinkeby',
+  localhost = 'Localhost',
   unknown = 'Unknown'
 }
 
 export enum Web3LoadingStatus {
   loading = 'Wallet loading is in progress',
-  noMetaMask = 'No wallet has been connected',
-  noAccountsAvailable = 'Your wallet is locked or there are no accounts available',
+  unableToConnectToSelectedNetwork = 'Unable to connect to the selected network.', ',
+  noMetaMask = 'Wallet is not connected.',
+  noAccountsAvailable = 'Your wallet is locked or there are no accounts available.',
+  wrongNetwork = 'Your wallet is connected to the wrong Network.',
   error = 'Something went wrong when connecting to your wallet',
   complete = 'Successfully connected to your wallet'
 }
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class EthService implements OnDestroy {
-
   web3js: any;
-  private accountInterval: any;
+  accountInterval: any;
   netType: NetworkType;
   walletType: WalletType;
+  ownerAccount: string;
 
-  private web3Status = new BehaviorSubject<Web3LoadingStatus>(Web3LoadingStatus.loading);
+  public web3Status = new BehaviorSubject<Web3LoadingStatus>(Web3LoadingStatus.loading);
   public web3Status$ = this.web3Status.asObservable();
 
   public account = new BehaviorSubject<string>(null);
   public account$ = this.account.asObservable();
 
   constructor(protected http: Http) {
-    if (typeof web3 !== 'undefined') {
-      this.web3js = new Web3(web3.currentProvider);
+    if (typeof window.ethereum !== 'undefined') {
+
+      this.web3js = new Web3(window.ethereum);
+
       this.setWalletType();
 
       try {
         this.web3js.eth.net.getId().then((id: number) => {
           console.log('Web3Service: Network retrieved: ID= ' + id);
+
           switch (id) {
             case 1:
               this.netType = NetworkType.main;
@@ -71,12 +78,19 @@ export class EthService implements OnDestroy {
         });
 
       } catch (e) {
+        console.log('Web3Service: Error: ID');
         this.web3Status.next(Web3LoadingStatus.error);
       }
     } else {
       this.web3js = new Web3();
       this.web3Status.next(Web3LoadingStatus.noMetaMask);
     }
+
+    setTimeout(() => {
+      if (!this.netType) {
+        this.web3Status.next(Web3LoadingStatus.unableToConnectToSelectedNetwork);
+      }
+    }, 5000);
   }
 
   ngOnDestroy() {
@@ -89,40 +103,54 @@ export class EthService implements OnDestroy {
     if (this.web3js.currentProvider.isMetaMask) {
       return this.walletType = WalletType.metaMask;
     }
+
     if (this.web3js.currentProvider.isTrust) {
       return this.walletType = WalletType.trust;
     }
+
     return this.walletType = null;
   }
 
   private setUpAccounts() {
-    console.log('Web3Service: Is: ', this.netType);
-    this.web3js.eth.getAccounts().then((accs: string[]) => {
-      console.log('Web3Service: Got accounts: ' + JSON.stringify(accs));
-      this.account.next(accs[0]);
-      if (accs[0] !== undefined) {
-        this.web3Status.next(Web3LoadingStatus.complete);
-      } else {
-        this.web3Status.next(Web3LoadingStatus.noAccountsAvailable);
-      }
-      this.accountInterval = setInterval(() => this.checkAccountMetaMask(), 5000);
-    });
+    if ((this.conf.useTestNet && (this.netType === NetworkType.rinkeby
+      || this.netType === NetworkType.ropsten || this.netType === NetworkType.unknown))
+      || (!this.conf.useTestNet && this.netType === NetworkType.main)) {
+      console.log('Web3Service: Is: ', this.netType);
+      this.web3js.eth.getAccounts().then((accs: string[]) => {
+        console.log('Web3Service: Got accounts: ' + JSON.stringify(accs));
+        this.account.next(accs[0]);
+        if (accs[0]) {
+          this.ownerAccount = accs[0];
+          this.web3Status.next(Web3LoadingStatus.complete);
+        } else {
+          window.ethereum.enable();
+          this.web3Status.next(Web3LoadingStatus.noAccountsAvailable);
+        }
+        this.accountInterval = setInterval(() => this.checkAccountMetaMask(), 5000);
+      });
+
+      return;
+    }
+
+    this.web3Status.next(Web3LoadingStatus.wrongNetwork);
   }
 
   private checkAccountMetaMask() {
     this.web3js.eth.getAccounts().then((accs: string[]) => {
       if (accs[0] !== this.account.value) {
         this.account.next(accs[0]);
-        if (accs[0] !== undefined) {
+        if (accs[0] !== undefined) { // && (this.web3Status.value !== Web3LoadingStatus.complete)
+          this.ownerAccount = accs[0];
           this.web3Status.next(Web3LoadingStatus.complete);
           return;
         }
+
         this.web3Status.next(Web3LoadingStatus.noAccountsAvailable);
       }
     });
   }
 
-  async getEthBalanceAsync(userAddress: string = this.account.value): Promise<string> {
+  async getEthBalanceAsync(userAddress: string = this.getOwnerAccount()): Promise<string> {
     const balance = await this.web3js.eth.getBalance(userAddress);
     if (balance) {
       console.log(balance);
@@ -134,6 +162,18 @@ export class EthService implements OnDestroy {
     return Promise.reject(null);
   }
 
+  getOwnerAccount() {
+    if (this.web3Status.value === Web3LoadingStatus.noAccountsAvailable) {
+      window.ethereum.enable().then(acc => {
+        return acc[0];
+      }).catch(e => {
+        return this.ownerAccount;
+      });
+    } else {
+      return this.ownerAccount;
+    }
+  }
+
   get defaultGasParam() {
     return gas;
   }
@@ -142,11 +182,13 @@ export class EthService implements OnDestroy {
     return new Promise((resolve, reject) => {
       try {
         this.http.get(gasStationApi).toPromise().then(res => {
-          if (res.status === 200) {
+          if (res.ok) {
             resolve(JSON.parse(res.text())['fast'].toString() + '00000000');
           } else {
             resolve('11000000000');
           }
+        }).catch(e => {
+          resolve('11000000000');
         });
       } catch (e) {
         resolve('11000000000');
@@ -154,37 +196,48 @@ export class EthService implements OnDestroy {
     });
   }
 
+
+  amountToERCTokens(amount, decimal) {
+    return amount * Math.pow(10, decimal);
+  }
+
   createContractInstance(abi, address) {
+    if (!this.web3js) {
+      console.log('Error createContractInstance, web3 provider not initialized');
+      return;
+    }
     return new this.web3js.eth.Contract(abi, address);
   }
 
-  payEth(recipient: string, amount: number, from = this.account.value): Promise<any> {
-    return new Promise(async (resolve, reject) => {
-      const gasPrice = await this.getDefaultGasPriceGwei();
-      const tx = await this.web3js.eth.sendTransaction({
-        from,
-        to: recipient,
-        value: this.web3js.utils.toWei(amount.toString()),
-        gas: 21001,
-        gasPrice
-      }, (err, txHash) => this.resolveTransaction(err, txHash, resolve, reject));
+  async payWithEther(amount: string, to: string): Promise<any> {
+    const gasPrice = await this.getDefaultGasPriceGwei();
+    const from = this.getOwnerAccount();
+    return new Promise((resolve, reject) => {
+      this.web3js.eth.sendTransaction({
+        to,
+        from: from,
+        value: this.web3js.utils.toWei(amount.toString(), 'ether'),
+        gasPrice: gasPrice
+      }, async (err, txHash) => this.resolveTransaction(err, from, txHash, resolve, reject));
     });
   }
 
-
-  async resolveTransaction(err, txHash, resolve, reject) {
+  async resolveTransaction(err, from, txHash, resolve, reject, onTxHash: Function = null) {
     if (err) {
       reject(err);
-    }
-    try {
-      const receipt = await this.getTransactionReceiptMined(txHash);
-      receipt.status = typeof (receipt.status) === 'boolean' ? receipt.status : this.web3js.utils.hexToNumber(receipt.status);
-      resolve(receipt);
-    } catch (e) {
-      reject(e);
+    } else {
+      try {
+        if (onTxHash) {
+          onTxHash(txHash, from);
+        }
+        const receipt = await this.getTransactionReceiptMined(txHash);
+        receipt.status = typeof (receipt.status) === 'boolean' ? receipt.status : this.web3js.utils.hexToNumber(receipt.status);
+        resolve(receipt);
+      } catch (e) {
+        reject(e);
+      }
     }
   }
-
 
   getTransactionReceiptMined(txHash, interval = 500, blockLimit = 0): Promise<any> {
     const transactionReceiptAsync = (resolve, reject) => {
@@ -192,12 +245,10 @@ export class EthService implements OnDestroy {
         if (error) {
           return reject(error);
         }
-
         if (receipt == null) {
           setTimeout(() => transactionReceiptAsync(resolve, reject), interval);
           return;
         }
-
         resolve(receipt);
       });
     };
